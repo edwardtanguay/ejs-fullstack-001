3fd64f09b7efe5310329e58c1dac5e3fbcb5ba34
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-06 22:29:04 +0100

    info done

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
 const showInfo = () => {
 	const productInfoElem = document.querySelector('.productInfo');
-	productInfoElem.innerHTML = 'This is the best book.';
+	productInfoElem.innerHTML = 'This is an old book (2013) but discusses topics such as document embedding, polymorphic schemas, and other MongoDB patterns for big data projects.';
 }
